a612eec3d0a035724a26de886658f4e3655c6ec6
aasaad <aasaad@sumerge.com>
2025-08-06 14:28:31 +0300

    feat: add reactotron

---
+++ ReactotronConfig.js
@@ -0,0 +1,25 @@
+import Reactotron from 'reactotron-react-native';
+
+// const middleware = tron => {
+//   /* custom plugin definition where you can add your own */
+// };
+
+Reactotron
+  .configure({
+    name: 'React Native - HUB',
+  })
+  .useReactNative({
+    networking: {
+      // optionally, you can turn it off with false.
+      ignoreUrls: /symbolicate/,
+    },
+    editor: true, // there are more options to editor
+    errors: { veto: stackFrame => false }, // or turn it off with false
+    overlay: false, // just turning off overlay,
+    log: true,
+  })
+  //   .use(middleware) // plus some custom made plugin. // uncomment the middleware an this line if you wish to add custom plugins.
+  .connect();
+
+  
+console.tron = Reactotron;
