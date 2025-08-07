ae7688aca63f6142197b64ee32e271ef52cef780
aasaad <aasaad@sumerge.com>
2025-08-07 10:38:03 +0300

    Merge remote-tracking branch 'refs/remotes/origin/main' into feature/linting

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { Pressable, Text, View } from 'react-native';
 import { useTheme } from './src/hooks/useTheme';
 import BaseText from './src/components/atoms/base-text/base-text.component';
 import { useTranslate } from './src/hooks/useTranslate';
+import Icon from './src/components/atoms/icon/icon.component';
+import { COLORS } from './src/constants/colors.constant';
 
 const App = () => {
   const { toggleTheme } = useTheme();
@@ -13,7 +15,7 @@ const App = () => {
       <BaseText
         text="common.welcome"
         textProps={{ name: 'hamada' }}
-        className="text-alpha-green-50 dark:text-primary-25">
+        className="text-alpha-green-50 dark:text-primary-25 paragraph-spacing-text-xl">
         <BaseText
           text="common.welcome common.obj.obj1"
           textProps={{ name: 'hamada' }}
@@ -21,13 +23,21 @@ const App = () => {
       </BaseText>
       <Pressable
         onPress={toggleTheme}
-        className="mt-3 py-2 bg-primary-600 dark:bg-gray-700 rounded">
-        <Text className="text-light-text dark:text-white">Toggle Theme</Text>
+        className="mt-3 py-2 bg-primary-600 dark:bg-gray-700 rounded ">
+        <Text className="text-light-text dark:text-white line-heights-text-xs">
+          Toggle Theme
+        </Text>
       </Pressable>
+      <Icon
+        name="bolt-circle"
+        size={300}
+        primaryColor={COLORS['secondary-yellow-300']}
+        secondaryColor={COLORS['secondary-yellow-800']}
+      />
       <Pressable
         onPress={() => changeLanguage(locale === 'ar' ? 'en' : 'ar')}
-        className="mt-4 ml-2 mr-2 px-4 py-2 bg-gray-200 dark:bg-gray-700">
-        <Text className="text-light-text dark:text-white text-60 font-regular">
+        className="mt-4 ml-2 mr-2 px-4 py-2 bg-gray-200 dark:bg-gray-700 rounded-8 border-al">
+        <Text className="text-light-text dark:text-white text-20 font-regular background-white">
           Toggle Lang
         </Text>
       </Pressable>
