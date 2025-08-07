570f1270f0132a60d281a3d2f719f65343e9f78e
kgamal <kgamal@sumerge.com>
2025-08-07 11:51:25 +0300

    fix: interface usage

--- src/components/atoms/base-toggle/index.tsx
+++ src/components/atoms/base-toggle/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-native';
-import ToggleProps from './base-toggle.props';
+import ToggleProps from './interface';
 
 function Toggle({ testId, ...toggleProps }: ToggleProps) {
   return <Switch testID={`${testId}-toggle`} {...toggleProps} />;
