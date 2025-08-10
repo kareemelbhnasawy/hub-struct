784de917e34cb930059aa8d2cdb776c2ee4331f0
ibrahemAssem <ibrahem.assem@hotmail.com>
2025-08-10 15:07:36 +0300

    feat: added ios config in firebase

--- App.tsx
+++ App.tsx
@@ -4,10 +4,19 @@ import { Pressable, Text, View } from 'react-native';
 import { LucideIcon, BaseText, MultiColorIcon } from '@/components/atoms';
 import { COLORS } from '@/constants';
 import { useTheme, useTranslate } from '@/hooks';
+import { crash, getCrashlytics } from '@react-native-firebase/crashlytics';
+import { useEffect } from 'react';
 
 const App = () => {
   const { toggleTheme } = useTheme();
   const { changeLanguage, locale } = useTranslate();
+
+  const crashlytics = getCrashlytics();
+
+  useEffect(() => {
+    if (crashlytics) crashlytics.log('App mounted.');
+  }, [crashlytics]);
+
   return (
     <View className="mt-96 bg-white dark:bg-black">
       <BaseText
@@ -37,6 +46,13 @@ const App = () => {
         size={100}
         color={COLORS['secondary-orange-900']}
       />
+      <Pressable
+        onPress={() => crash(crashlytics)}
+        className="mt-4 ml-2 mr-2 px-4 py-2 bg-gray-200 dark:bg-gray-700 rounded-8 border-al">
+        <Text className="text-center text-light-text dark:text-white text-20 font-regular background-white">
+          CRASH MY APP
+        </Text>
+      </Pressable>
       <Pressable
         onPress={() => changeLanguage(locale === 'ar' ? 'en' : 'ar')}
         className="mt-4 ml-2 mr-2 px-4 py-2 bg-gray-200 dark:bg-gray-700 rounded-8 border-al">
