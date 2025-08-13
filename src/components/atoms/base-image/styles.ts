39b86511936a54b1b46856da471d60b002032799
alafty <mohamedelalafty@yahoo.com>
2025-08-13 15:30:21 +0300

    fix: added responsive handler

--- src/components/atoms/base-image/styles.ts
+++ src/components/atoms/base-image/styles.ts
@@ -1,11 +1,17 @@
 import { StyleSheet } from 'react-native';
+import { responsiveHandler } from '@/theme';
 
 export const styles = StyleSheet.create({
-  fullSize: {
-    height: '100%',
-    width: '100%',
-  },
-  roundedFull: {
-    borderRadius: 9999,
-  },
+  fullSize: responsiveHandler({
+    base: {
+      height: '100%',
+      width: '100%',
+
+    }
+  }),
+  roundedFull: responsiveHandler({
+    base: {
+      borderRadius: 9999,
+    }
+  }),
 });
