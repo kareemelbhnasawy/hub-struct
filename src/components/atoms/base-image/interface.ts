8ff30d9584111a3a6d421ad59e144f72e053730f
alafty <mohamedelalafty@yahoo.com>
2025-08-13 12:08:56 +0300

    fix: renamed sizes to sm md and lg

--- src/components/atoms/base-image/interface.ts
+++ src/components/atoms/base-image/interface.ts
@@ -2,7 +2,7 @@ import { ImageProps, ImageSourcePropType } from 'react-native';
 
 interface BaseImageProps extends ImageProps {
   testId: string;
-  size: string;
+  size: 'sm' | 'md' | 'lg' | 'xl';
   image: string | ImageSourcePropType;
   resizeMode?: 'cover' | 'contain' | 'stretch' | 'repeat' | 'center';
   roundedImageSize?: number;
--- src/components/molecules/avatar/index.tsx
+++ src/components/molecules/avatar/index.tsx
@@ -1,12 +1,10 @@
 import AvatarProps from './interface';
-import { getRelativeAvatarSizes } from './utils';
 import { getInitialsFromName } from './utils';
 import { View } from 'react-native';
 import BaseImage from '../../atoms/base-image';
 import { Paragraph } from '@/components/atoms';
 
 const Avatar = ({ testId, size, name, image }: AvatarProps) => {
-  const [relativeSize, typoSize] = getRelativeAvatarSizes(size);
 
   return (
     <View testID={`${testId}-avatar`}>
@@ -15,12 +13,12 @@ const Avatar = ({ testId, size, name, image }: AvatarProps) => {
           testId={testId}
           isCircular
           image={image}
-          size={relativeSize}
+          size={size}
         />
       ) : (
         // TODO: Change base Text to Text Component
         <Paragraph
-          size={typoSize}
+          size={size}
           weight="Bold"
           text={getInitialsFromName(name ?? '')}
         />
