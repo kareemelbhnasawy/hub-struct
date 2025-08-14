e7c523350c9d4ace92bfad2454805e09981bc0aa
alafty <mohamedelalafty@yahoo.com>
2025-08-14 17:51:41 +0300

    feat: developed a bottom sheet

---
+++ src/components/molecules/base-sheet/interface.ts
@@ -0,0 +1,16 @@
+import DisplayProps from '@/components/atoms/typography/headline/interface';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { StyleProp, ViewStyle } from 'react-native';
+
+interface BaseSheetProps {
+    testId: string;
+    children: React.ReactNode;
+    sheetRef: React.RefObject<BottomSheet | null>;
+    snapPoints: string[];
+    titleProps?: DisplayProps;
+    hasCloseButton?: boolean;
+    hasSubmitButton?: boolean;
+    containerStyle?: StyleProp<ViewStyle>;
+}
+
+export default BaseSheetProps;
