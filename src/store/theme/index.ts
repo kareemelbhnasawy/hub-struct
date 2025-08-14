30ca5197cf8e3aa01486595225ccf167fa99b3b3
Adel Asaad <adel0asaad@gmail.com>
2025-08-14 12:16:09 +0300

    fix: persist

--- src/store/theme/index.ts
+++ src/store/theme/index.ts
@@ -1,35 +1,34 @@
 import { create } from 'zustand';
 import ThemeState, { RNStyle, ThemeMode } from './interface';
 import { Appearance } from 'react-native';
-import { getString, setString } from '@/utilities';
-import { STORAGE_KEYS } from '@/constants/storageKeys';
 import { getThemeColor, responsiveHandler } from './utils';
+import { persist } from 'zustand/middleware';
+import { getMMKVStorage } from '../mmkv-storage';
+import { STORAGE_KEYS } from '@/constants/storageKeys';
 
 const systemScheme = Appearance.getColorScheme();
-const storedTheme = getString(STORAGE_KEYS.COLOR_SCHEME) as ThemeMode | null;
-const initialTheme: ThemeMode =
-  storedTheme || (systemScheme as ThemeMode) || 'light';
+const initialTheme: ThemeMode = (systemScheme as ThemeMode) || 'light';
 
-export const useThemeStore = create<ThemeState>((set, get) => ({
-  theme: initialTheme,
-  toggleTheme: () =>
-    set((prevState) => {
-      const newTheme = prevState.theme === 'light' ? 'dark' : 'light';
-      setString(STORAGE_KEYS.COLOR_SCHEME, newTheme);
-      return {
-        theme: newTheme,
-      };
+const useThemeStore = create<ThemeState>()(
+  persist(
+    (set, get) => ({
+      theme: initialTheme,
+      toggleTheme: () =>
+        set((prevState) => ({
+          theme: prevState.theme === 'light' ? 'dark' : 'light',
+        })),
+      setTheme: (theme) => set({ theme }),
+      getThemeColor: (colorKey) => getThemeColor(colorKey, get().theme),
+      getThemedStyles: (styles) => {
+        const themedStyles: Record<string, RNStyle> = {};
+        for (const key in styles) {
+          themedStyles[key] = responsiveHandler(styles[key], get().theme);
+        }
+        return themedStyles;
+      },
     }),
-  setTheme: (theme) => {
-    setString(STORAGE_KEYS.COLOR_SCHEME, theme);
-    return set({ theme });
-  },
-  getThemeColor: (colorKey) => getThemeColor(colorKey, get().theme),
-  getThemedStyles: (styles) => {
-    const themedStyles: Record<string, RNStyle> = {};
-    for (const key in styles) {
-      themedStyles[key] = responsiveHandler(styles[key], get().theme);
-    }
-    return themedStyles;
-  },
-}));
+    { name: STORAGE_KEYS.COLOR_SCHEME, storage: getMMKVStorage<ThemeState>() },
+  ),
+);
+
+export { useThemeStore };
