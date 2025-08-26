f5375f935d7a74d72e8b8163d92d28cc6d57eddf
alafty <mohamedelalafty@yahoo.com>
2025-08-26 15:47:39 +0300

    feat: handled on success and on error upon submit

--- src/navigation/stacks/auth/screens/login/index.tsx
+++ src/navigation/stacks/auth/screens/login/index.tsx
@@ -5,16 +5,62 @@ import { Form, Page } from '@/components/templates';
 import { styles } from './styles';
 import { useThemeStore } from '@/store/theme';
 import { useNavigation } from '@/hooks';
+import useLoginStart from '@/network/services/auth/login-start/login-start.hook';
+import { FormikValues } from 'formik';
+import { getMMKVStorage } from '@/store/mmkv-storage';
+import { useState } from 'react';
+import { log } from '@/utilities';
+import { ToastService } from '@/components/molecules';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
   const screenTestId = 'login-screen';
   const { getThemedStyles } = useThemeStore();
   const themedStyles = getThemedStyles(styles);
+  const mmkv = getMMKVStorage<string>();
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   const handleNavigateToApp = () => {
     navigation.navigate('App');
   };
+
+  const onLoginSuccess = () => {
+    //save data
+    mmkv.setItem('email', { state: email, version: Date.now() });
+    mmkv.setItem('password', { state: password, version: Date.now() });
+    log(mmkv.getItem('email'));
+    log(mmkv.getItem('password'));
+
+    //navigate to OTP
+    handleNavigateToApp();
+  };
+
+  const onLoginError = () => {
+    // Show Toast
+    ToastService.error({
+        props: {
+          messageProps: { text: 'auth.invalidData' },
+          testId: '',
+        },
+        duration: 4000,
+      });
+  };
+
+  const { mutate: login } = useLoginStart(onLoginSuccess, onLoginError);
+
+  const onSubmit = (values: FormikValues) => {
+    setEmail(values.mail);
+    setPassword(values.password);
+    const loginRequest = {
+      email: values.mail,
+      password: values.password,
+      deviceId: 'some-device-id', // Replace with actual deviceId logic if needed
+    };
+
+    login(loginRequest);
+  };
+
   return (
     <Page testId={screenTestId} hasHeader={false}>
       <Spacer space={50} />
@@ -25,7 +71,6 @@ const LoginScreen = () => {
         text="auth.login"
         size="lg"
         weight="Semibold"
-        //todo: add color
         style={themedStyles.defaultText}
       />
       <Headline
@@ -33,11 +78,12 @@ const LoginScreen = () => {
         text="auth.description"
         size="xs"
         weight="Medium"
+        style={themedStyles.defaultText}
       />
       <Spacer space={40} />
       <Form
         testId={screenTestId}
-        onSubmit={() => handleNavigateToApp()}
+        onSubmit={onSubmit}
         fields={[
           {
             name: 'mail',
