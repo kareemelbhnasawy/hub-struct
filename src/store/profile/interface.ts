b0b0ebe01f81c1786a212070022069215800fb21
alafty <mohamedelalafty@yahoo.com>
2025-09-02 13:35:17 +0300

    feat: created a profile store

---
+++ src/store/profile/interface.ts
@@ -0,0 +1,26 @@
+interface ProfileState {
+  name: string;
+  kunya: string;
+  avatarUrl: string;
+  bannerId: string;
+  status: string;
+  jobTitle: string;
+  department: string;
+  getFullName: () => object;
+  getName: () => string;
+  getKunya: () => string;
+  getAvatarUrl: () => string;
+  getBannerId: () => string;
+  getStatus: () => string;
+  getJobTitle: () => string;
+  getDepartment: () => string;
+  setName: (name: string) => void;
+  setFullName: (name: string, kunya: string) => void;
+  setAvatarUrl: (url: string) => void;
+  setBannerId: (id: string) => void;
+  setStatus: (status: string) => void;
+  setJobTitle: (title: string) => void;
+  setDepartment: (department: string) => void;
+
+}
+export default ProfileState;
