74d6b6e6fe968f18f7637d1bba2f54a45193631e
alafty <mohamedelalafty@yahoo.com>
2025-09-09 15:49:29 +0300

    feat: created checkmark component

---
+++ src/components/molecules/checkmark/index.tsx
@@ -0,0 +1,52 @@
+import { CheckmarkProps } from './interface';
+import { Pressable, View } from 'react-native';
+import styles from './styles';
+import { useThemeStore } from '@/store/theme';
+import { useState } from 'react';
+import { LucideIcon, Headline } from '@/components/atoms';
+
+const Checkmark = ({
+  testId,
+  textProps,
+  disabled,
+  onCheck,
+  onUncheck,
+  containerStyle,
+}: CheckmarkProps) => {
+  const { getThemedStyles } = useThemeStore();
+  const themedStyles = getThemedStyles(styles);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handlePress = () => {
+    if (onCheck && !isClicked) {
+      onCheck();
+    }
+    if (onUncheck && isClicked) {
+      onUncheck();
+    }
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <Pressable
+      testID={`${testId}-checkmark`}
+      style={[themedStyles.container, containerStyle]}
+      onPress={!disabled ? handlePress : undefined}>
+      <View style={[themedStyles.checkmark, isClicked && themedStyles.checked]}>
+        {isClicked && (
+          <LucideIcon
+            name="Check"
+            color="backgroundWhite"
+            size={16}
+            testId={testId}
+          />
+        )}
+      </View>
+      {textProps && (
+        <Headline testId={testId} size="2xs" weight="Medium" {...textProps} />
+      )}
+    </Pressable>
+  );
+};
+
+export default Checkmark;
---
+++ src/components/molecules/checkmark/interface.ts
@@ -0,0 +1,12 @@
+import HeadlineProps from '@/components/atoms/typography/headline/interface';
+import { RNStyle } from '@/types/themes';
+
+export interface CheckmarkProps {
+  testId: string;
+  hasText?: boolean;
+  textProps?: Omit<HeadlineProps, 'testId'>;
+  disabled?: boolean;
+  containerStyle?: RNStyle;
+  onCheck?: () => void;
+  onUncheck?: () => void;
+}
