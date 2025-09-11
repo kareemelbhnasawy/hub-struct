42eb30110607ec7867b223d91915de6eab1aa0e2
kgamal <kgamal@sumerge.com>
2025-09-11 15:29:28 +0300

    feat: debugging isRTL, lang

--- src/screens/profile/home/index.tsx
+++ src/screens/profile/home/index.tsx
@@ -3,13 +3,21 @@ import { Pressable, Text, View } from 'react-native';
 import styles from './styles';
 import TeamMemberItemDemo from '../partials/team-member-item/demo';
 import { useNavigation, useTranslate } from '@/hooks';
+import { ToastService } from '@/components/molecules';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const handleNavigateToProfile = () => {
+    ToastService.info({
+      props: {
+        messageProps: { text: 'Language is ' + locale + ' isRTL is ' + isRTL },
+        testId: '',
+      },
+    });
     navigation.navigate('ProfileStack', { userId: '123' });
   };
-  const { changeLanguage } = useTranslate();
+  const { changeLanguage, locale, isRTL } = useTranslate();
+  console.log(locale);
   // changeLanguage('ar');
   return (
     <View style={styles.container.base}>
