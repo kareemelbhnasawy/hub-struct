ca5884f437be2a4176d5f01926b60f9d4fc8fd24
aasaad <aasaad@sumerge.com>
2025-09-14 16:43:20 +0300

    fix: add workplace settings and declarations

--- declaration.d.ts
+++ declaration.d.ts
@@ -4,3 +4,12 @@ declare module '*.svg' {
   const content: React.FC<SvgProps>;
   export default content;
 }
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
