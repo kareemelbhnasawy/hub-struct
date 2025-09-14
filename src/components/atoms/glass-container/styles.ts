fd56bc7bf480f9e717fb3820d6a7805f1533b266
kgamal <kgamal@sumerge.com>
2025-09-14 15:46:23 +0300

    feat: back button brighter

--- src/components/atoms/glass-container/styles.ts
+++ src/components/atoms/glass-container/styles.ts
@@ -9,7 +9,7 @@ const styles = (borderRadius: Radius) =>
         paddingVertical: 12,
         borderRadius: borderRadius,
         overflow: 'hidden',
-        backgroundColor: 'alphaBlack10',
+        backgroundColor: 'alphaWhite50',
         justifyContent: 'center',
         alignItems: 'center',
         ...SHADOWS.md,
